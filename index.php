f030c3d9993785821b008eef430d2473c333b1bc
Enis Kurtay YILMAZ <kurtayyilmaz@gmail.com>
2019-07-29 12:07:19 +0300

    added saveClientCard example

--- index.php
+++ index.php
@@ -30,6 +30,7 @@ try {
     $responseUserCheck = $client->UserCheck();
     var_dump($responseUserCheck);
 
+
     /* Executing a fuction, for example GetClientCardList*/
     $responseGetClientCardList = $client->GetClientCardList();
     $array = json_decode(json_encode($responseGetClientCardList), true);
@@ -38,14 +39,70 @@ try {
 
     echo  "Liste sonucundaki ilk kartın adı : <b>" . $array['GetClientCardListResult']['ClientCard']['0']['DEFINITION_'] . "</b>";
 
-    /* Details */ 
+    /* Details of GetClientCardList */
     echo '<br><br><br>';
     foreach ($array as $item) {
         echo '<pre>';
         var_dump($item);
     }
-
     print_r($array);
+
+    
+    /* Create a new client card*/
+    $arrayClientCard = array(
+        "ClientCard" => array(
+            "LOGICALREF" =>  "0",
+            "ISPERSCOMP" =>  "1",
+            "CODE" =>  "CODE111",
+            "DEFINITION_" =>  "CARI UNVAN",
+            "DEFINITION2" =>  "CARI UNVAN 2",
+            "NAME" =>  "",
+            "SURNAME" =>  "",
+            "ADDR1" =>  "",
+            "ADDR2" =>  "",
+            "TOWNCODE" =>  "",
+            "TOWN" =>  "",
+            "CITYCODE" =>  "",
+            "CITY" =>  "",
+            "COUNTRYCODE" =>  "",
+            "COUNTRY" =>  "",
+            "POSTCODE" =>  "",
+            "TELCODES1" =>  "",
+            "TELCODES2" =>  "",
+            "TELNRS1" =>  "",
+            "TELNRS2" =>  "",
+            "TELEXTNUMS1" =>  "",
+            "TELEXTNUMS2" =>  "",
+            "FAXCODE" =>  "",
+            "FAXNR" =>  "",
+            "FAXEXTNUM" =>  "",
+            "TCKNO" =>  "17768815565",
+            "TAXNR" =>  "",
+            "TAXOFFICE" =>  "",
+            "INCHARGE" =>  "",
+            "INCHARGE2" =>  "",
+            "INCHARGE3" =>  "",
+            "EMAILADDR" =>  "",
+            "EMAILADDR2" =>  "",
+            "EMAILADDR3" =>  "",
+            "CYPHCODE" =>  "",
+            "SPECODE" =>  "",
+            "SPECODE2" =>  "",
+            "SPECODE3" =>  "",
+            "SPECODE4" =>  "",
+            "SPECODE5" =>  "",
+            "EXTACCESSFLAGS" =>  "",
+            "ACCEPTEINV" =>  "",
+            "PURCHBRWS" =>  "1",
+            "SALESBRWS" =>  "1",
+            "IMPBRWS" =>  "1",
+            "EXPBRWS" =>  "1",
+            "FINBRWS" =>  "1"
+        )
+    );
+    $responseNewClientCard = $client->SaveClientCard($arrayClientCard);
+    var_dump($responseNewClientCard);
+
 } catch (Exception $e) {
     echo $e->getMessage();
 }
